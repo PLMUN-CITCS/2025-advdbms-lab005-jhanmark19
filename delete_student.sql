19e3ff5898fee4322ac13ac1fe316c38125e5230
jhanmark19 <loretejhanmark_bsit@plmun.edu.ph>
2025-02-20 15:26:47 +0800

    lab005

--- delete_student.sql
+++
@@ -1,4 +0,0 @@
-USE `UniversityDB`;
-
-DELETE FROM `Students`
-WHERE `FirstName` = 'Charlie' AND `LastName` = 'Lee';
--- update_student_email.sql
+++
@@ -1,5 +0,0 @@
-USE `UniversityDB`;
-
-UPDATE `Students`
-SET `Email` = 'bob.j@example.com'
-WHERE `FirstName` = 'Bob' AND `LastName` = 'Johnson';
